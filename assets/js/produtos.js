36c9a3e1e0d9fa5e87ac79dbdc316d95e5d9ab06
gustavosgouveia <gustavomsgouveia1@hotmail.com>
2023-08-27 21:14:09 -0300

    att

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -8,6 +8,35 @@ if(localStorage.getItem("token") == null){
     window.location.href = "../html/index.html"
 }
 
+
+function salvar(){
+
+    let idCodigo = document.getElementById("idCodigo")
+    let nomeProduto = document.getElementById("nomeProduto")
+    let precoCusto = document.getElementById("precoCusto")
+    let precoVenda = document.getElementById("precoVenda")
+    let quantidadeEstoque = document.getElementById("quantidadeEstoque")
+    let unidadeMedida = document.getElementById("unidadeMedida")
+    let informacoesAdicionais = document.getElementById("informacoesAdicionais")
+
+    let dados = JSON.parse(localStorage.getItem("dadosProduto"))
+    if(dados == null){
+        localStorage.setItem("dadosProduto", "[]")
+        dados = []
+    }
+    let auxRegistro = {
+        id: idCodigo.value, 
+        nome: nomeProduto.value,
+        precoC: precoCusto.value,
+        precoV: precoVenda.value,
+        quantidade: quantidadeEstoque.value,
+        unidade: unidadeMedida.value,
+        informacoes: informacoesAdicionais.value
+    }
+    dados.push(auxRegistro)
+    localStorage.setItem("dadosProduto", JSON.stringify(dados))
+}
+
 function sair(){
     localStorage.removeItem("token")
     window.location.href = "../html/index.html"
